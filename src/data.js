61b0e585544489686102acec012fc4d59f5fb8cb
DominicBarnett <dominic.barnett@icloud.com>
2025-03-31 17:11:15 -0700

    up to 1.4 done

---
+++ src/data.js
@@ -0,0 +1,22 @@
+import data from './data.json' assert { type: "json" };
+export default data;
+
+const categories = [...new Set(data.map(product => product.category))];
+// const uniqueCategories = [...new Set(categories)]
+// console.log(categories); 
+
+const categoryCounts = data.reduce((acc, product) => {
+    acc[product.category] = (acc[product.category] || 0) + 1;
+    return acc;
+  }, {});
+  
+//   console.log(categoryCounts);
+
+  const categoryNameCounts = Object.entries(
+    data.reduce((acc, product) => {
+      acc[product.category] = (acc[product.category] || 0) + 1;
+      return acc;
+    }, {})
+  ).map(([name, count]) => ({ name, count }));
+  
+//   console.log(categoryNameCounts);
